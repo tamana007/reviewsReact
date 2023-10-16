d1e1213abea74e359f818c5f88543c79510d247b
tamana007 <tamana.efatwira1@gmail.com>
2023-10-16 12:12:15 -0700

    third

--- Review.js
+++ Review.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import reviews from './data';
 
 const Review = () => {
-  return <h2>review component</h2>;
+
+  return (<><h2>review component</h2>;
+  {reviews.map((item)=>{
+    return(
+      <div className='review'>{item.name}</div>
+    )
+  })}
+  <button className='remove-btn'>add</button>
+  
+  </>)
 };
 
 export default Review;

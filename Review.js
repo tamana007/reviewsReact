21982846cea31994ff826ffedfa1c2029964d2c9
tamana007 <tamana.efatwira1@gmail.com>
2023-10-16 12:59:39 -0700

    fourth

--- Review.js
+++ Review.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 import reviews from './data';
 
 const Review = () => {
 
-  return (<><h2>review component</h2>;
-  {reviews.map((item)=>{
-    return(
-      <div className='review'>{item.name}</div>
-    )
-  })}
-  <button className='remove-btn'>add</button>
+  const [people,setPeople]=useState(0)
+  const {id,name,job,image,text}=reviews[people];
+
+  useEffect(()=>{
+    function changeReview(){
+    setPeople(1)
+    }
+  },[people])
+
+  // function changeReview(){
+    
   
-  </>)
+  console.log(people);
+
+  return (<article className='review'>
+    <div className='img-container'>
+      <img className='person-img' src={image}></img>
+      <span className='quote-icon'>
+        <FaQuoteRight/>
+      </span>
+    </div>
+    {/* <button onClick={changeReview}>click me</button> */}
+
+  </article>
+  
+  )
 };
 
 export default Review;
